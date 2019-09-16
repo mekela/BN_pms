4d6ef449565f7213a05d9319a0ce21c997dc000a
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-09-16 23:21:28 +0300

    desctop

---
+++ app/js/common.js
@@ -0,0 +1,30 @@
+//scroll
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 500) {
+        $(".header").addClass("fixed");
+    } else {
+        $(".header").removeClass("fixed");
+    }
+});
+
+//timer
+var endDate = new Date("Oct 25, 2019 12:00:00").getTime();
+var timer = setInterval(function () {
+let now = new Date().getTime();
+let t = endDate - now;
+if (t >= 0) {
+document.getElementById("remainder").innerHTML = "<div class='timer_item'>" +
+Math.floor(t / (1000 * 60 * 60 * 24)) + "<span class='label'>[Days]</span></div>" + "<div class='timer_item'>" +
+("0" + Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2) + "<span class='label'>[Hours]</span></div>" + "<div class='timer_item'>" +
+("0" + Math.floor((t % (1000 * 60 * 60)) / (1000 * 60))).slice(-2) + "<span class='label'>[Minutes]</span></div>" + "<div class='timer_item'>" +
+("0" + Math.floor((t % (1000 * 60)) / 1000)).slice(-2) + "<span class='label'>[Seconds]</span></div>";
+} else {
+document.getElementById("remainder").innerHTML = "End of timer.";
+}
+
+}, 1000);
+
+
+
